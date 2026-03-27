a16a532d194fbc151715d08fa31ca69bfe8bc932
Zehranur Gülşin <zehragulsin00@gmail.com>
2026-03-27 16:45:35 +0300

    Integrate Firebase initialization in main function
    Initialize Firebase in the main function before running the app.

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'register_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +20,6 @@ class MyApp extends StatelessWidget {
   // Renkleri sabitledik, gri tonları ve arka plan
   static const Color primaryColor = Color(0xFF757575); // Gri
   static const Color backgroundColor = Color(0xFFFFFFFF); // Beyaz
-
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +37,9 @@ class MyApp extends StatelessWidget {
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             backgroundColor: primaryColor,
-            shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
             padding: const EdgeInsets.symmetric(vertical: 15),
           ),
         ),
